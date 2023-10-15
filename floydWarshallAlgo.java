8ed25656cdc1d33650ed659c0a59e593b95a3519
aryan <91269171+Ryaaan4321@users.noreply.github.com>
2023-10-15 15:05:48 +0530

    initial commit

---
+++ floydWarshallAlgo.java
@@ -0,0 +1,46 @@
+public class floydWarshallAlgo {
+    public static void shortestPath(int[][] matrix) {
+        int n = matrix.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = (int) 1e9;
+                }
+                if (i == j) matrix[i][j] = 0;
+            }
+        }
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    matrix[i][j] = Math.min(matrix[i][j], matrix[i][k] + matrix[k][j]);
+                }
+            }
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == (int) 1e9) {
+                    matrix[i][j] = -1;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = {
+            {0, 2, -1, -1},
+            {1, 0, 3, -1},
+            {-1, -1, 0, -1},
+            {3, 5, 4, 0}
+        };
+
+        shortestPath(matrix);
+
+        
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
